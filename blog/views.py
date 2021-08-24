442e7f3300b21d0199b5743a7b250bcc6fc327b6
stephen.rivas <stephenfrivas@gmail.com>
2021-08-23 20:24:33 -0700

    First commit. This project has been completed up to video 11

---
+++ blog/views.py
@@ -0,0 +1,100 @@
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from .models import Post
+
+
+# Create your views here.
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    # See tutorial Part 10
+    # default template_name django looks for is <app>/<model>_<viewtype>.html
+    # e.g. blog/post_list.html
+    template_name = 'blog/home.html'
+    # See tutorial Part 10
+    # django default calls context variable 'object'
+    context_object_name = 'posts'
+    # See tutorial Part 10
+    # orders the DB query from oldest to newest by default.
+    # The prepended '-' reverses that order (i.e. newest to oldest)
+    ordering = ['-date_posted']
+    # See tutorial Part 11
+    # sets how many objects to show on each page when paginating
+    paginate_by = 5
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title':'About'})
+
+
+# Types of Views
+# List
+# Detail
+# Create
+# Update
+# Delete
