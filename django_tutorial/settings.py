4d72729d020d107eb5c37c3cdb10d999ac37170f
stephen.rivas <stephenfrivas@gmail.com>
2021-09-08 10:20:57 -0700

    Following last CoreyMS video for hosting on Heroku. Also modified settings.py to connect to a PostgreSQL DB

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -79,13 +79,25 @@ WSGI_APPLICATION = 'django_tutorial.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# Default DB configuration for Django
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# Adding PostgreSQL DB connection
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_tutorial',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
